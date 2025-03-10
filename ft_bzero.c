b1bd5daf2210d1f8134f4c721e2a579440da72c5
Florian Braune <fbraune@3-F-6.42heilbronn.de>
2025-03-10 15:15:35 +0100

    bzero and memset

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,15 +6,15 @@
 /*   By: fbraune <fbraune@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 15:09:16 by fbraune           #+#    #+#             */
-/*   Updated: 2025/03/10 15:14:45 by fbraune          ###   ########.fr       */
+/*   Updated: 2025/03/10 15:15:11 by fbraune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *bezero(void *s, size_t n)
+void	*ft_bezero(void *s, size_t n)
 {
-    unsigned char	*ptr;
+	unsigned char	*ptr;
 
 	ptr = (unsigned char *)s;
 	while (n-- >= 1)
