111a7683a19d6180572632b6baad86e4d52390d7
Florian Braune <fbraune@3-E-8.42heilbronn.de>
2025-03-18 17:17:37 +0100

    added test for lstiter

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: fbraune <fbraune@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 18:12:36 by fbraune           #+#    #+#             */
-/*   Updated: 2025/03/16 18:56:19 by fbraune          ###   ########.fr       */
+/*   Updated: 2025/03/18 16:51:49 by fbraune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,40 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*))
 	del (lst->content);
 	free (lst);
 }
+
+// void del (void *content)
+// {
+// 	free (content);
+// }
+
+// int main()
+// {
+// 	t_list *head = NULL;
+// 	char *str1 = ft_strdup("1");
+// 	char *str2 = ft_strdup("2");
+// 	t_list *node1 = ft_lstnew(str1);
+// 	t_list *node2 = ft_lstnew(str2);
+// 	ft_lstadd_back(&head, node1);
+// 	ft_lstadd_back(&head, node2);
+// 	t_list *temp = head;
+
+// 	while (temp != NULL)
+// 	{
+// 		printf("%s\n", (char *)temp->content);
+// 		temp = temp->next;
+// 	}
+// 	node1->next = NULL;
+// 	ft_lstdelone(node2, del);
+// 	temp = head;
+// 	while (temp != NULL)
+// 	{
+// 		printf("%s\n", (char *)temp->content);
+// 		temp = temp->next;
+// 	}
+// 	while (head != NULL)
+// 	{
+// 		t_list *next = head->next;
+// 		ft_lstdelone(head, del);
+// 		head = next;
+// 	}
+// }
--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: fbraune <fbraune@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 18:49:00 by fbraune           #+#    #+#             */
-/*   Updated: 2025/03/18 15:40:26 by fbraune          ###   ########.fr       */
+/*   Updated: 2025/03/18 17:15:47 by fbraune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,3 +25,20 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 		cur = cur->next;
 	}
 }
+
+// void print(void *content)
+// {
+// 	printf("Content: %s\n", (char *)content);
+// }
+
+// int main()
+// {
+// 	t_list *head = NULL;
+
+// 	t_list *node1 = ft_lstnew("1");
+// 	t_list *node2 = ft_lstnew("2");
+// 	ft_lstadd_back(&head, node1);
+// 	ft_lstadd_back(&head, node2);
+// 	t_list *temp = head;
+// 	ft_lstiter(head, print);
+// }
