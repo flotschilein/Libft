1f8349d705e4348d1a5a71cfcac7c6cf71ddce90
Florian Braune <fbraune@3-E-8.42heilbronn.de>
2025-03-18 16:16:52 +0100

    fixed lstclear + tests

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: fbraune <fbraune@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 18:18:36 by fbraune           #+#    #+#             */
-/*   Updated: 2025/03/16 18:46:44 by fbraune          ###   ########.fr       */
+/*   Updated: 2025/03/18 16:15:01 by fbraune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,24 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 		*lst = cur;
 	}
 }
+
+// void del (void *content)
+// {
+// 	free (content);
+// }
+
+// int main()
+// {
+// 	t_list *head = NULL;
+// 	char *str1 = ft_strdup("1");
+// 	char *str2 = ft_strdup("2");
+// 	t_list *node1 = ft_lstnew(str1);
+// 	t_list *node2 = ft_lstnew(str2);
+// 	ft_lstadd_back(&head, node1);
+// 	ft_lstadd_back(&head, node2);
+// 	ft_lstclear(&head, del);
+// 	if (head == NULL)
+// 		printf ("list cleared \n");
+// 	else
+// 		printf("list not cleared \n");
+// }
