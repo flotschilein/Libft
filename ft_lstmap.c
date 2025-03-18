f43cc06be28464988419198db4c9edb484fc7456
Florian Braune <fbraune@3-E-8.42heilbronn.de>
2025-03-18 17:37:28 +0100

    test for lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: fbraune <fbraune@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 18:58:15 by fbraune           #+#    #+#             */
-/*   Updated: 2025/03/17 15:24:38 by fbraune          ###   ########.fr       */
+/*   Updated: 2025/03/18 17:36:48 by fbraune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,3 +36,39 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (res_lst);
 }
+
+// void *to_uppercase(void *content) {
+// 	int i;
+
+// 	i = 0;
+// 	char *str = ft_strdup((char *)content);
+// 	while (str[i] != '\0')
+// 	{
+// 		str[i] = ft_toupper(str[i]);
+// 		i++;
+// 	}
+// 	return (str);
+// }
+
+// void del(void *content)
+// {
+// 	free(content);
+// }
+
+// int main()
+// {
+// 	t_list *head = NULL;
+// 	t_list *res	 = NULL;
+
+// 	t_list *node1 = ft_lstnew("sgsd3QWdsdd");
+// 	t_list *node2 = ft_lstnew("uduifiuydgshHYFsyuaud78387");
+// 	ft_lstadd_back(&head, node1);
+// 	ft_lstadd_back(&head, node2);
+// 	res = ft_lstmap(head, to_uppercase, del);
+// 	while (res != NULL)
+// 	{
+// 		printf("%s\n", (char *)res->content);
+// 		res = res->next;
+// 	}
+// }
+
